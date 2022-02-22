666d1e9a87078aae40de716061bf288ec36e295b
pell13 <peixueying@gmail.com>
2022-02-22 16:23:48 +0900

    Add : navigator bar

---
+++ dart_tutorial/dart_tutorial_001/bin/dart_tutorial_001.dart
@@ -0,0 +1,9 @@
+import 'package:dart_tutorial_001/dart_tutorial_001.dart' as dart_tutorial_001;
+import 'dart:io';
+
+void main() {
+  stdout.writeln('What is yout name : ? ');
+  String? name = stdin.readLineSync();
+  
+  print('Hello ' + '${name}');
+}

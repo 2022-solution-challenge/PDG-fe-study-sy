1c1c8fc64b494907a33cf3c24354588f75c81771
Seolyeong Bae <80435616+pell13@users.noreply.github.com>
2022-03-07 20:42:05 +0900

    Main view basic setting (#4)
    * Add : Horizontal Slider Widget in mainPage
    
    * Try : remove margin at first/last component in carouselSlider
    
    * Add : Horizontal ListView
    
    * Fix : move HomePage Widget to home folder
    
    * Add : Main Layout Frame
    
    * Add : Record Layout, put dummy data
    
    * Add : Icons in AppBar
    
    * Add : Half Background by using Stack Widget()

--- flutter_test_app/lib/main.dart
+++ flutter_test_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home/home-page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -16,17 +17,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('This is a Home Page'),
-    );
-  }
-}
-
 class UserPage extends StatelessWidget {
   const UserPage({Key? key}) : super(key: key);
 
@@ -37,7 +27,6 @@ class UserPage extends StatelessWidget {
     );
   }
 }
-
 
 class MyStatefulWidget extends StatefulWidget {
   const MyStatefulWidget({Key? key}) : super(key: key);
@@ -63,12 +52,51 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Navi App'),
+
+     var size = MediaQuery.of(context)
+        .size; //this gonna give us total height and with of our device
+
+    
+    AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Color.fromARGB(255, 255, 155, 155),
+      elevation: 0,
+      leading: IconButton(
+        icon: Icon(Icons.backspace),
+        onPressed: () {},
       ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.home),
+          onPressed: () {},
+        ),
+        SizedBox(width: 20.0 / 2)
+      ],
+    );
+  }
+    return Scaffold(
+      appBar: buildAppBar(),
+      body:Stack(
+        children: <Widget>[
+          Container(
+            // Here the height of the container is 45% of our total height
+            height: size.height * .20,
+            decoration: BoxDecoration(
+              color: Color(0xFFF5CEB8),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Center(
+                child: _widgetOptions.elementAt(_selectedIndex),
+                )
+            )
+          )]
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -86,5 +114,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         onTap: _onItemTapped,
       ),
     );
+
+    
   }
 }
